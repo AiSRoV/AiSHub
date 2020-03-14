72df4bcb19775112284e3c7d52f0ef3d2f47de3e
AiSRoV <55707801+AiSRoV@users.noreply.github.com>
2020-03-14 13:58:49 +0300

    Add files via upload

---
+++ Labs/Lab1/Lab2/Lab2 Code.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    double a, b ,x1, x2, d, z, xma, xmi, x;
+
+    bool flag = true;
+
+    cin >> a >> b >> x1 >> x2 >> d;
+    cout << "|  " << 'x' << " | " << 'y' << " |" <<  endl << "|-----|-----|" << endl;
+
+    x = x1;
+    xma = 0;
+    xmi = 0;
+
+    for (int i = 0; i <= (x2 -x1)/d; i ++, x += d){
+
+        if (x <= a) z = abs(x);
+        else if ((a < x)&& (x < b)) z = abs(x) + cos(x);
+        else if( x >= b) z = tan(x);
+
+        if (flag) {
+            xma = z;
+            xmi = z;
+            flag = false;
+        } else if (z > xma) xma = z;
+        else if (z < xmi) xmi = z;
+
+        cout <<  "|  " << x << " | " << z << " |" <<  endl;
+    }
+    cout << endl << "max: " << xma << "  min: " << xmi << endl;
+
+    return 0;
+}
